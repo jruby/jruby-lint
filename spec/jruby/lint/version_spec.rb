a9a6242b7aaceb07797b3f2918055bff842c48f2
Nick Sieger <nick@nicksieger.com>
2011-05-05 18:06:23 -0500

    Doc change for version_spec

--- spec/jruby/lint/version_spec.rb
+++ spec/jruby/lint/version_spec.rb
@@ -1,6 +1,6 @@
 require File.expand_path('../../../spec_helper', __FILE__)
 
-describe JRuby::Lint do
+describe JRuby::Lint, "version" do
   Given(:version) { JRuby::Lint::VERSION }
   Then { version.should >= "0" }
 end
