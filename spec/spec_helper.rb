6be2fa6dd4d1df5c6a5230f5f65282a854c81bb0
Nick Sieger <nick@nicksieger.com>
2011-05-05 12:24:23 -0500

    Add Aruba for CLI specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,16 @@
 require 'rspec'
 require 'rspec/given'
 require 'jruby/lint'
+require 'aruba/api'
+
+module JRuby::Lint::Specs
+  PROJECT_DIR = File.expand_path('../..', __FILE__)
+  def project_dir
+    PROJECT_DIR
+  end
+end
+
+RSpec.configure do |config|
+  config.include JRuby::Lint::Specs
+  config.include Aruba::Api
+end
