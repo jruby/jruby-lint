b26cdc9c0412dd23cfe7b6fb038ad4ae03577fd6
Nick Sieger <nick@nicksieger.com>
2011-05-09 16:42:19 -0500

    Make a lexical namespace for JRuby::Lint

--- lib/jruby/lint/collector.rb
+++ lib/jruby/lint/collector.rb
@@ -1,14 +1,16 @@
-class JRuby::Lint::Collector
-  attr_accessor :checkers, :reporters, :findings
+module JRuby::Lint
+  class Collector
+    attr_accessor :checkers, :reporters, :findings
 
-  def initialize
-    @checkers  = JRuby::Lint::Checker.loaded_checkers.map(&:new)
-    @reporters = []
-    @findings  = []
-  end
+    def initialize
+      @checkers  = Checker.loaded_checkers.map(&:new)
+      @reporters = []
+      @findings  = []
+    end
 
-  def run
-    checkers.each {|c| c.check(self) }
-    reporters.each {|r| r.report(findings)}
+    def run
+      checkers.each {|c| c.check(self) }
+      reporters.each {|r| r.report(findings)}
+    end
   end
 end
