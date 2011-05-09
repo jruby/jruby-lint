89edc3c65051b24fd6bda38aae3e41d4581ca71c
Nick Sieger <nick@nicksieger.com>
2011-05-09 15:10:35 -0500

    Reorder givens closer to the specs that use them

--- spec/jruby/lint/collector_spec.rb
+++ spec/jruby/lint/collector_spec.rb
@@ -3,15 +3,16 @@ require File.expand_path('../../../spec_helper', __FILE__)
 describe JRuby::Lint::Collector do
   Given(:collector) { JRuby::Lint::Collector.new }
   Given(:checker) { mock("checker").tap {|checker| collector.checkers = [checker] } }
-  Given(:reporter) { mock("reporter").tap {|reporter| collector.reporters = [reporter] } }
-  Given(:finding) { mock "finding" }
 
-  it "should invoke all checkers" do
+  it "invokes all checkers" do
     checker.should_receive(:check).with(collector)
     collector.run
   end
 
-  it "should report all findings" do
+  Given(:reporter) { mock("reporter").tap {|reporter| collector.reporters = [reporter] } }
+  Given(:finding) { mock "finding" }
+
+  it "reports all findings" do
     checker.stub!(:check).and_return do
       collector.findings << finding
     end
